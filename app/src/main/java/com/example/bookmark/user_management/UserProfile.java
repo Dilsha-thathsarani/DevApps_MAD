2b252e11f828fc6607b587bf5930fa3860f2b5d1
Dulanjana9914 <it20265892@my.sliit.lk>
2021-09-21 02:37:28 +0530

    Reg & Login

--- app/src/main/java/com/example/bookmark/user_management/UserProfile.java
+++ app/src/main/java/com/example/bookmark/user_management/UserProfile.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.bookmark.R;
 
-public class UserActivity extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_user_profile);
     }
 }
