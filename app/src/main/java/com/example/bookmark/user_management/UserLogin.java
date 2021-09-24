49f484fff65e664852c6fa5fdd39e140c089c351
Dulanjana9914 <it20265892@my.sliit.lk>
2021-09-25 03:33:43 +0530

    Profile

--- app/src/main/java/com/example/bookmark/user_management/UserLogin.java
+++ app/src/main/java/com/example/bookmark/user_management/UserLogin.java
@@ -1,19 +1,19 @@
 package com.example.bookmark.user_management;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.bookmark.R;
+import com.example.bookmark.product_review.ReviewBooks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -60,51 +60,50 @@ public class UserLogin extends AppCompatActivity {
                     progressDialog.setCanceledOnTouchOutside(false);
                     progressDialog.show();
 
-                   if(task.isSuccessful()){
+                    if(task.isSuccessful()){
+
+                        String uid= Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()).getUid();
 
 
-                       String uid=task.getResult().getUser().getUid();
+                        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+                        firebaseDatabase.getReference().child("Users").child(uid).child("userType").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                int userType= dataSnapshot.getValue(int.class);
 
+                                if(userType==0){
 
-                       FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-                       firebaseDatabase.getReference().child("Users").child(uid).child("userType").addListenerForSingleValueEvent(new ValueEventListener() {
-                           @Override
-                           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                               int userType= dataSnapshot.getValue(int.class);
+                                    progressDialog.dismiss();
+                                    Intent intent=new Intent(UserLogin.this, ReviewBooks.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    Toast.makeText(UserLogin.this,"Successfully Logged In",Toast.LENGTH_LONG).show();
+                                }
+                                if(userType==1){
 
-                               if(userType==0){
+                                    progressDialog.dismiss();
+                                    Intent intent=new Intent(UserLogin.this,UserProfile.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    Toast.makeText(UserLogin.this,"Successfully Logged In As Admin",Toast.LENGTH_LONG).show();
 
-                                   progressDialog.dismiss();
-                                   Intent intent=new Intent(UserLogin.this,UserProfile.class);
-                                   intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                   startActivity(intent);
-                                   Toast.makeText(UserLogin.this,"Successfully Logged In",Toast.LENGTH_LONG).show();
-                               }
-                               if(userType==1){
-
-                                   progressDialog.dismiss();
-                                   Intent intent=new Intent(UserLogin.this,UserProfile.class);
-                                   intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                   startActivity(intent);
-                                   Toast.makeText(UserLogin.this,"Successfully Logged In As Admin",Toast.LENGTH_LONG).show();
-
-                               }
+                                }
 
 
 
-                           }
+                            }
 
-                           @Override
-                           public void onCancelled(@NonNull DatabaseError databaseError) {
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
 
 
-                           }
-                       });
+                            }
+                        });
 
-                   }else{
-                       progressDialog.dismiss();
-                       Toast.makeText(UserLogin.this,""+task.getException(),Toast.LENGTH_LONG).show();
-                   }
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(UserLogin.this,""+task.getException(),Toast.LENGTH_LONG).show();
+                    }
 
                 });
 
