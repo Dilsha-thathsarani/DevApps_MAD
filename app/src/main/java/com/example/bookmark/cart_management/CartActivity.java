35cc5d2fb760d24ca803cf62864a99d8ab046163
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-09-25 21:01:58 +0530

    edit/delete cart

--- app/src/main/java/com/example/bookmark/cart_management/CartActivity.java
+++ app/src/main/java/com/example/bookmark/cart_management/CartActivity.java
@@ -1,5 +1,7 @@
 package com.example.bookmark.cart_management;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,17 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bookmark.R;
 import com.example.bookmark.cart_management.Models.Cart;
+import com.example.bookmark.cart_management.Prevalent.Prevalent;
 import com.example.bookmark.cart_management.ViewHolder.CartViewHolder;
+import com.example.bookmark.listing_management.ListingActivity;
+import com.example.bookmark.listing_management.ProductDetailsActivity;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -70,6 +79,54 @@ public class CartActivity extends AppCompatActivity {
                 Log.d(TAG, "onBindViewHolder: " + holder.txtProductName + "" + holder.txtProductPrice + "" + holder.txtProductPrice + "");
 
                 //Log.d(TAG, "CartViewHolder: "+txtProductName+""+txtProductPrice+""+txtProductQuantity+"");
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CharSequence options[] = new CharSequence[]
+                                {
+                                        "Edit",
+                                        "Remove"
+                                };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(CartActivity.this);
+                        builder.setTitle("Card Option:");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                if(i == 0){
+                                    Intent intent = new Intent(CartActivity.this, ProductDetailsActivity.class);
+                                    intent.putExtra("pid",model.getPid());
+                                    startActivity(intent);
+                                }
+                                if(i== 1){
+                                    cartListRef.child("User View")
+                                            .child(Prevalent.currentOnlineUser.getPhone())
+                                            .child("Product")
+                                            .child(model.getPid())
+                                            .removeValue()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                                    if(task.isSuccessful())
+                                                    {
+                                                        Toast.makeText(CartActivity.this, "Item removed successfully", Toast.LENGTH_SHORT).show();
+/****************************   redirect after delete  ***************************************************************************************/
+                                                        Intent intent = new Intent(CartActivity.this, ListingActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                }
+                                            });
+                                }
+
+                            }
+                        });
+
+                        builder.show();
+                    }
+                });
+
+
 
             }
 
