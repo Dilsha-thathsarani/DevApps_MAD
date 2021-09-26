6000e165df26613ed23c47a61958db13b916ce44
Sanjula Dulshan <77122922+Sanjula-Dulshan@users.noreply.github.com>
2021-09-26 11:12:35 +0530

    Delete app directory

--- app/src/main/java/com/example/bookmark/cart_management/Interface/ItemClickListner.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmark.cart_management.Interface;
-
-import android.view.View;
-
-public interface ItemClickListner {
-    void onClick(View v, int position, boolean isLongClick);
-}
--- app/src/main/java/com/example/bookmark/cart_management/Prevalent/Prevalent.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookmark.cart_management.Prevalent;
-
-import com.example.bookmark.user_management.User;
-
-public class Prevalent {
-    public static User currentOnlineUser;
-
-    private static final String UserPhonekey = "phone";
-}
